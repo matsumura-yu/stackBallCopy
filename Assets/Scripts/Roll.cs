81967e9c26ba762a9b943fafc4b5c2812590287a
matsumura-yu <matsumura.yu@isid.co.jp>
2020-06-04 23:16:26 +0900

    Add roll and create stage

--- Assets/Scripts/Roll.cs
+++ Assets/Scripts/Roll.cs
@@ -7,12 +7,6 @@ public class Roll : MonoBehaviour
     [SerializeField]
     private float rotateSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Scripts/StageCreate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCreate : MonoBehaviour
+{
+    // ステージの段数
+    public int plateNum;
+
+    // ステージのスタートポジション
+    public GameObject startPos;
+
+    // ステージのゴール
+    public GameObject goalPos;
+
+    // 一段のオブジェクト
+    public GameObject plate;
+
+    // plateの差分
+    public float plateOffset;
+
+    // plateの回転角差分
+    public float angleOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 startPosVec = startPos.transform.position;
+
+        for(int i=0; i < plateNum; ++i)
+        {
+            Instantiate(plate, startPosVec - (Vector3.up * plateOffset * i) ,Quaternion.Euler(0, -angleOffset * i, 0), transform);
+        }
+    }
+
+    
+}
