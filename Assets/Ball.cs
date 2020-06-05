e81a11f97ddee4f4cb13bfb1a38988e6fac9659f
matsumura-yu <matsumura.yu@isid.co.jp>
2020-06-05 17:24:43 +0900

    WIP

---
+++ Assets/Ball.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float jumpHeight;
+
+    public bool onClicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            onClicked = true;
+        }
+        else
+        {
+            Debug.Log("false");
+            onClicked = false;
+        }
+    }
+
+    void Jump()
+    {
+        // TODO Jumpのアニメーションも書きたい
+        // rb.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
+        rb.velocity = Vector3.up * jumpHeight;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (onClicked)
+        {
+            if (collision.gameObject.CompareTag("Soft"))
+            {
+                // TODO Break処理
+                Destroy(collision.gameObject.transform.parent.gameObject);
+            }
+            else if (collision.gameObject.CompareTag("Hard"))
+            {
+                Debug.Log("game over");
+            }
+        }
+        else
+        {
+            Jump();
+        }
+    }
+}
