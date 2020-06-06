608d9ce6ba09572f0fccdde77299b415edcbe145
matsumura-yu <matsumura.yu@isid.co.jp>
2020-06-06 17:23:06 +0900

    Add gameManager and UIManager

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ゲームのレベル
+public enum GameState
+{
+    Waiting,
+    Playing,
+    Failed,
+    Success
+}
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager Instance
+    {
+        get; private set;
+    }
+
+    // 今のゲームレベル
+    public int level;
+
+    public GameState currentGameState;   
+
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        level = 1;
+        currentGameState = GameState.Waiting;
+    }
+
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text levelText;
+    public Text underText;
+    public Text messageText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SceneManager.Instance.currentGameState == GameState.Waiting)
+        {
+            levelText.gameObject.SetActive(false);
+            messageText.gameObject.SetActive(false);
+            underText.gameObject.SetActive(true);
+            underText.text = "TAP TO PLAY";
+            return;
+        }
+
+        if(SceneManager.Instance.currentGameState == GameState.Playing)
+        {
+            levelText.gameObject.SetActive(false);
+            messageText.gameObject.SetActive(false);
+            underText.gameObject.SetActive(false);
+            return;
+        }
+
+        if (SceneManager.Instance.currentGameState == GameState.Failed)
+        {
+            levelText.gameObject.SetActive(true);
+            levelText.text = "SCORE:" + SceneManager.Instance.level;
+            messageText.gameObject.SetActive(true);
+            messageText.text = "GAME OVER";
+            underText.gameObject.SetActive(true);
+            underText.text = "TOUCH TO RESTART";
+            return;
+        }
+
+        if (SceneManager.Instance.currentGameState == GameState.Success)
+        {
+            levelText.gameObject.SetActive(true);
+            levelText.text = "Level " + SceneManager.Instance.level;
+            messageText.gameObject.SetActive(true);
+            messageText.text = "COMPLETED!";
+            underText.gameObject.SetActive(true);
+            underText.text = "TOUCH TO NEXT";
+            return;
+        }
+    }
+}
