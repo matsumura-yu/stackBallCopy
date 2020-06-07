3e4bab57a03928d54d734e28bbd209c0899391ac
matsumura-yu <matsumura.yu@isid.co.jp>
2020-06-07 13:58:56 +0900

    Add circle progress

---
+++ Assets/CircleProgress.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CircleProgress : MonoBehaviour
+{
+    public Image circle;
+
+    public Ball ball;
+
+    private float time;
+
+    public float startTime = 0.25f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (ball == null) return;
+        if (ball.isBurst || ball.successTime > startTime)
+        {
+            time = ball.successTime / ball.burstTime;
+        }
+        // 0~1までで表す
+        circle.fillAmount = time;
+    }
+}
