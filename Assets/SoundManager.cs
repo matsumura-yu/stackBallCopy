5c1b85f074a660e5e63ced0aed56e5dd0c4e3531
matsumura-yu <matsumura.yu@isid.co.jp>
2020-06-07 15:09:08 +0900

    Test sound

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("sound");
+            jump.PlayOneShot(jump.clip);
+            //jump.Play();
+        }
+    }
+}
